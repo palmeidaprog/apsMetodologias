7f46f3946bead29c32ffb7c01ab8317803f1e7f0
Paulo Almeida Filho <palmeidaprogramming@gmail.com>
2020-05-07 17:56:19 -0300

    Implementando final do teste

--- src/app/teste/teste.component.ts
+++ src/app/teste/teste.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit, ViewChild} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {FormControl, FormGroup} from '@angular/forms';
 import {PreRequisito} from '../pre-requisito.enum';
 import {Tamanho} from '../tamanho.enum';
@@ -185,8 +185,447 @@ export class TesteComponent implements OnInit {
     this.alert();
   }
 
+  private resetaPontos(): void {
+    this.xp.pontos = 0;
+    this.rup.pontos = 0;
+    this.componentes.pontos = 0;
+    this.cascata.pontos = 0;
+    this.scrum.pontos = 0;
+    this.kanban.pontos = 0;
+  }
+
   private inicializaPesos(): void {
     this.pesosKanban();
+    this.pesosScrum();
+    this.pesosXP();
+    this.pesosComponentes();
+    this.pesosRUP();
+    this.pesosCascata();
+  }
+
+  private pesosCascata(): void {
+    this.cascata.pratica = [
+      new Opcao(Pratica.TDD, 0),
+      new Opcao(Pratica.PROGRAMACAO_PARES, 5),
+      new Opcao(Pratica.PADRONIZACAO_CODIGO, 5)
+    ];
+
+    this.cascata.comunicacao = [
+      new Opcao(Intensidade.FRACA, 10),
+      new Opcao(Intensidade.MEDIA, 10),
+      new Opcao(Intensidade.FORTE, 10)
+    ];
+
+    this.cascata.segurancaVidaHumana = [
+      new Opcao(Booleana.SIM, 20),
+      new Opcao(Booleana.NAO, 0)
+    ];
+
+    this.cascata.equipeIndependente = [
+      new Opcao(Booleana.SIM, 10),
+      new Opcao(Booleana.NAO, 10)
+    ];
+
+    this.cascata.utilizacaoParcial = [
+      new Opcao(Booleana.SIM, 0),
+      new Opcao(Booleana.NAO, 20)
+    ];
+
+    this.cascata.extensaoTempo = [
+      new Opcao(Extensao.CURTO, 20),
+      new Opcao(Extensao.MEDIA, 10),
+      new Opcao(Extensao.LONGO, 5)
+    ];
+
+    this.cascata.preReq = [
+      new Opcao(PreRequisito.QUASE_ESTATICO, 20),
+      new Opcao(PreRequisito.POUCO, 10),
+      new Opcao(PreRequisito.MODERADO, 0),
+      new Opcao(PreRequisito.VOLTATIL, 0)
+    ];
+
+    this.cascata.tamanhoEquipe = [
+      new Opcao(Tamanho.PEQUENA, 10),
+      new Opcao(Tamanho.MEDIA, 10),
+      new Opcao(Tamanho.GRANDE, 10)
+    ];
+
+    this.cascata.multidisciplinar = [
+      new Opcao(Booleana.SIM, 0),
+      new Opcao(Booleana.NAO, 10)
+    ];
+
+    this.cascata.donoDoProduto = [
+      new Opcao(Booleana.SIM, 10),
+      new Opcao(Booleana.NAO, 10)
+    ];
+
+    this.cascata.multiFoco = [
+      new Opcao(Booleana.SIM, 0),
+      new Opcao(Booleana.NAO, 10)
+    ];
+
+    this.cascata.documentacao = [
+      new Opcao(Booleana.SIM, 20),
+      new Opcao(Booleana.NAO, 0)
+    ];
+
+    this.cascata.escritorio = [
+      new Opcao(Booleana.SIM, 20),
+      new Opcao(Booleana.NAO, 0)
+    ];
+
+    this.cascata.linear = [
+      new Opcao(Booleana.SIM, 20),
+      new Opcao(Booleana.NAO, 0)
+    ];
+
+    // cultura da empreas para modificar metodologias
+    this.cascata.probabilidade = [
+      new Opcao(Probabilidade.IMPROVAVEL, 20),
+      new Opcao(Probabilidade.POSSIVEL, 10),
+      new Opcao(Probabilidade.MUITO_PROVAVEL, 0)
+    ];
+  }
+
+  private pesosRUP(): void {
+    this.rup.pratica = [
+      new Opcao(Pratica.TDD, 20),
+      new Opcao(Pratica.PROGRAMACAO_PARES, 10),
+      new Opcao(Pratica.PADRONIZACAO_CODIGO, 20)
+    ];
+
+    this.rup.comunicacao = [
+      new Opcao(Intensidade.FRACA, 0),
+      new Opcao(Intensidade.MEDIA, 10),
+      new Opcao(Intensidade.FORTE, 20)
+    ];
+
+    this.rup.segurancaVidaHumana = [
+      new Opcao(Booleana.SIM, 0),
+      new Opcao(Booleana.NAO, 5)
+    ];
+
+    this.rup.equipeIndependente = [
+      new Opcao(Booleana.SIM, 10),
+      new Opcao(Booleana.NAO, 5)
+    ];
+
+    this.rup.utilizacaoParcial = [
+      new Opcao(Booleana.SIM, 10),
+      new Opcao(Booleana.NAO, 5)
+    ];
+
+    this.rup.extensaoTempo = [
+      new Opcao(Extensao.CURTO, 0),
+      new Opcao(Extensao.MEDIA, 10),
+      new Opcao(Extensao.LONGO, 20)
+    ];
+
+    this.rup.preReq = [
+      new Opcao(PreRequisito.QUASE_ESTATICO, 10),
+      new Opcao(PreRequisito.POUCO, 10),
+      new Opcao(PreRequisito.MODERADO, 10),
+      new Opcao(PreRequisito.VOLTATIL, 10)
+    ];
+
+    this.rup.tamanhoEquipe = [
+      new Opcao(Tamanho.PEQUENA, 10),
+      new Opcao(Tamanho.MEDIA, 10),
+      new Opcao(Tamanho.GRANDE, 10)
+    ];
+
+    this.rup.multidisciplinar = [
+      new Opcao(Booleana.SIM, 5),
+      new Opcao(Booleana.NAO, 10)
+    ];
+
+    this.rup.donoDoProduto = [
+      new Opcao(Booleana.SIM, 10),
+      new Opcao(Booleana.NAO, 10)
+    ];
+
+    this.rup.multiFoco = [
+      new Opcao(Booleana.SIM, 10),
+      new Opcao(Booleana.NAO, 10)
+    ];
+
+    this.rup.documentacao = [
+      new Opcao(Booleana.SIM, 20),
+      new Opcao(Booleana.NAO, 0)
+    ];
+
+    this.rup.escritorio = [
+      new Opcao(Booleana.SIM, 10),
+      new Opcao(Booleana.NAO, 10)
+    ];
+
+    this.rup.linear = [
+      new Opcao(Booleana.SIM, 10),
+      new Opcao(Booleana.NAO, 10)
+    ];
+
+    // cultura da empreas para modificar metodologias
+    this.rup.probabilidade = [
+      new Opcao(Probabilidade.IMPROVAVEL, 5),
+      new Opcao(Probabilidade.POSSIVEL, 10),
+      new Opcao(Probabilidade.MUITO_PROVAVEL, 10)
+    ];
+  }
+
+  private pesosComponentes(): void {
+    this.componentes.pratica = [
+      new Opcao(Pratica.TDD, 10),
+      new Opcao(Pratica.PROGRAMACAO_PARES, 20),
+      new Opcao(Pratica.PADRONIZACAO_CODIGO, 10)
+    ];
+
+    this.componentes.comunicacao = [
+      new Opcao(Intensidade.FRACA, 0),
+      new Opcao(Intensidade.MEDIA, 10),
+      new Opcao(Intensidade.FORTE, 20)
+    ];
+
+    this.componentes.segurancaVidaHumana = [
+      new Opcao(Booleana.SIM, 0),
+      new Opcao(Booleana.NAO, 5)
+    ];
+
+    this.componentes.equipeIndependente = [
+      new Opcao(Booleana.SIM, 10),
+      new Opcao(Booleana.NAO, 5)
+    ];
+
+    this.componentes.utilizacaoParcial = [
+      new Opcao(Booleana.SIM, 10),
+      new Opcao(Booleana.NAO, 10)
+    ];
+
+    this.componentes.extensaoTempo = [
+      new Opcao(Extensao.CURTO, 5),
+      new Opcao(Extensao.MEDIA, 10),
+      new Opcao(Extensao.LONGO, 20)
+    ];
+
+    this.componentes.preReq = [
+      new Opcao(PreRequisito.QUASE_ESTATICO, 20),
+      new Opcao(PreRequisito.POUCO, 10),
+      new Opcao(PreRequisito.MODERADO, 10),
+      new Opcao(PreRequisito.VOLTATIL, 5)
+    ];
+
+    this.componentes.tamanhoEquipe = [
+      new Opcao(Tamanho.PEQUENA, 10),
+      new Opcao(Tamanho.MEDIA, 10),
+      new Opcao(Tamanho.GRANDE, 10)
+    ];
+
+    this.componentes.multidisciplinar = [
+      new Opcao(Booleana.SIM, 10),
+      new Opcao(Booleana.NAO, 10)
+    ];
+
+    this.componentes.donoDoProduto = [
+      new Opcao(Booleana.SIM, 10),
+      new Opcao(Booleana.NAO, 10)
+    ];
+
+    this.componentes.multiFoco = [
+      new Opcao(Booleana.SIM, 10),
+      new Opcao(Booleana.NAO, 10)
+    ];
+
+    this.componentes.documentacao = [
+      new Opcao(Booleana.SIM, 20),
+      new Opcao(Booleana.NAO, 5)
+    ];
+
+    this.componentes.escritorio = [
+      new Opcao(Booleana.SIM, 20),
+      new Opcao(Booleana.NAO, 5)
+    ];
+
+    this.componentes.linear = [
+      new Opcao(Booleana.SIM, 10),
+      new Opcao(Booleana.NAO, 10)
+    ];
+
+    // cultura da empreas para modificar metodologias
+    this.componentes.probabilidade = [
+      new Opcao(Probabilidade.IMPROVAVEL, 5),
+      new Opcao(Probabilidade.POSSIVEL, 10),
+      new Opcao(Probabilidade.MUITO_PROVAVEL, 10)
+    ];
+  }
+
+  private pesosXP(): void {
+    this.xp.pratica = [
+      new Opcao(Pratica.TDD, 20),
+      new Opcao(Pratica.PROGRAMACAO_PARES, 20),
+      new Opcao(Pratica.PADRONIZACAO_CODIGO, 20)
+    ];
+
+    this.xp.comunicacao = [
+      new Opcao(Intensidade.FRACA, 0),
+      new Opcao(Intensidade.MEDIA, 10),
+      new Opcao(Intensidade.FORTE, 20)
+    ];
+
+    this.xp.segurancaVidaHumana = [
+      new Opcao(Booleana.SIM, 0),
+      new Opcao(Booleana.NAO, 5)
+    ];
+
+    this.xp.equipeIndependente = [
+      new Opcao(Booleana.SIM, 20),
+      new Opcao(Booleana.NAO, 5)
+    ];
+
+    this.xp.utilizacaoParcial = [
+      new Opcao(Booleana.SIM, 10),
+      new Opcao(Booleana.NAO, 5)
+    ];
+
+    this.xp.extensaoTempo = [
+      new Opcao(Extensao.CURTO, 5),
+      new Opcao(Extensao.MEDIA, 10),
+      new Opcao(Extensao.LONGO, 10)
+    ];
+
+    this.xp.preReq = [
+      new Opcao(PreRequisito.QUASE_ESTATICO, 10),
+      new Opcao(PreRequisito.POUCO, 10),
+      new Opcao(PreRequisito.MODERADO, 10),
+      new Opcao(PreRequisito.VOLTATIL, 10)
+    ];
+
+    this.xp.tamanhoEquipe = [
+      new Opcao(Tamanho.PEQUENA, 20),
+      new Opcao(Tamanho.MEDIA, 10),
+      new Opcao(Tamanho.GRANDE, 10)
+    ];
+
+    this.xp.multidisciplinar = [
+      new Opcao(Booleana.SIM, 0),
+      new Opcao(Booleana.NAO, 20)
+    ];
+
+    this.xp.donoDoProduto = [
+      new Opcao(Booleana.SIM, 20),
+      new Opcao(Booleana.NAO, 5)
+    ];
+
+    this.xp.multiFoco = [
+      new Opcao(Booleana.SIM, 0),
+      new Opcao(Booleana.NAO, 20)
+    ];
+
+    this.xp.documentacao = [
+      new Opcao(Booleana.SIM, 10),
+      new Opcao(Booleana.NAO, 0)
+    ];
+
+    this.xp.escritorio = [
+      new Opcao(Booleana.SIM, 20),
+      new Opcao(Booleana.NAO, 5)
+    ];
+
+    this.xp.linear = [
+      new Opcao(Booleana.SIM, 5),
+      new Opcao(Booleana.NAO, 10)
+    ];
+
+    // cultura da empreas para modificar metodologias
+    this.xp.probabilidade = [
+      new Opcao(Probabilidade.IMPROVAVEL, 0),
+      new Opcao(Probabilidade.POSSIVEL, 10),
+      new Opcao(Probabilidade.MUITO_PROVAVEL, 20)
+    ];
+  }
+
+  private pesosScrum(): void {
+    this.scrum.pratica = [
+      new Opcao(Pratica.TDD, 10),
+      new Opcao(Pratica.PROGRAMACAO_PARES, 10),
+      new Opcao(Pratica.PADRONIZACAO_CODIGO, 5)
+    ];
+
+    this.scrum.comunicacao = [
+      new Opcao(Intensidade.FRACA, 0),
+      new Opcao(Intensidade.MEDIA, 10),
+      new Opcao(Intensidade.FORTE, 20)
+    ];
+
+    this.scrum.segurancaVidaHumana = [
+      new Opcao(Booleana.SIM, 0),
+      new Opcao(Booleana.NAO, 5)
+    ];
+
+    this.scrum.equipeIndependente = [
+      new Opcao(Booleana.SIM, 20),
+      new Opcao(Booleana.NAO, 0)
+    ];
+
+    this.scrum.utilizacaoParcial = [
+      new Opcao(Booleana.SIM, 20),
+      new Opcao(Booleana.NAO, 5)
+    ];
+
+    this.scrum.extensaoTempo = [
+      new Opcao(Extensao.CURTO, 5),
+      new Opcao(Extensao.MEDIA, 10),
+      new Opcao(Extensao.LONGO, 10)
+    ];
+
+    this.scrum.preReq = [
+      new Opcao(PreRequisito.QUASE_ESTATICO, 10),
+      new Opcao(PreRequisito.POUCO, 10),
+      new Opcao(PreRequisito.MODERADO, 20),
+      new Opcao(PreRequisito.VOLTATIL, 20)
+    ];
+
+    this.scrum.tamanhoEquipe = [
+      new Opcao(Tamanho.PEQUENA, 5),
+      new Opcao(Tamanho.MEDIA, 10),
+      new Opcao(Tamanho.GRANDE, 10)
+    ];
+
+    this.scrum.multidisciplinar = [
+      new Opcao(Booleana.SIM, 20),
+      new Opcao(Booleana.NAO, 10)
+    ];
+
+    this.scrum.donoDoProduto = [
+      new Opcao(Booleana.SIM, 20),
+      new Opcao(Booleana.NAO, 0)
+    ];
+
+    this.scrum.multiFoco = [
+      new Opcao(Booleana.SIM, 5),
+      new Opcao(Booleana.NAO, 10)
+    ];
+
+    this.scrum.documentacao = [
+      new Opcao(Booleana.SIM, 5),
+      new Opcao(Booleana.NAO, 20)
+    ];
+
+    this.scrum.escritorio = [
+      new Opcao(Booleana.SIM, 10),
+      new Opcao(Booleana.NAO, 10)
+    ];
+
+    this.scrum.linear = [
+      new Opcao(Booleana.SIM, 0),
+      new Opcao(Booleana.NAO, 20)
+    ];
+
+    // cultura da empreas para modificar metodologias
+    this.scrum.probabilidade = [
+      new Opcao(Probabilidade.IMPROVAVEL, 0),
+      new Opcao(Probabilidade.POSSIVEL, 10),
+      new Opcao(Probabilidade.MUITO_PROVAVEL, 20)
+    ];
   }
 
   private pesosKanban(): void {
@@ -255,6 +694,143 @@ export class TesteComponent implements OnInit {
       new Opcao(Booleana.SIM, 0),
       new Opcao(Booleana.NAO, 10)
     ];
+
+    this.kanban.escritorio = [
+      new Opcao(Booleana.SIM, 10),
+      new Opcao(Booleana.NAO, 10)
+    ];
+
+    this.kanban.linear = [
+      new Opcao(Booleana.SIM, 5),
+      new Opcao(Booleana.NAO, 10)
+    ];
+
+    // cultura da empreas para modificar metodologias
+    this.kanban.probabilidade = [
+      new Opcao(Probabilidade.IMPROVAVEL, 0),
+      new Opcao(Probabilidade.POSSIVEL, 10),
+      new Opcao(Probabilidade.MUITO_PROVAVEL, 10)
+    ];
+  }
+
+  calculate(): void {
+    this.inicializaPesos();
+    this.resetaPontos();
+
+    if (this.segurancaVidaHumanaCtrl.value ||
+      !this.utilizacaoParcialCtrl.value) {
+      this.cascata.pontos = 100;
+      this.cascata.percentual = 100;
+      return;
+    }
+
+    this.kanban.praticaResposta = this.praticaCtrl.value;
+    this.scrum.praticaResposta = this.praticaCtrl.value;
+    this.xp.praticaResposta = this.praticaCtrl.value;
+    this.componentes.praticaResposta = this.praticaCtrl.value;
+    this.rup.praticaResposta = this.praticaCtrl.value;
+    this.cascata.praticaResposta = this.praticaCtrl.value;
+
+    this.kanban.comunicacaoResposta = this.comunicacaoCtrl.value;
+    this.scrum.comunicacaoResposta = this.comunicacaoCtrl.value;
+    this.xp.comunicacaoResposta = this.comunicacaoCtrl.value;
+    this.componentes.comunicacaoResposta = this.comunicacaoCtrl.value;
+    this.rup.comunicacaoResposta = this.comunicacaoCtrl.value;
+    this.cascata.comunicacaoResposta = this.comunicacaoCtrl.value;
+
+    this.kanban.segurancaVidaHumanaResposta = this.segurancaVidaHumanaCtrl.value;
+    this.scrum.segurancaVidaHumanaResposta = this.segurancaVidaHumanaCtrl.value;
+    this.xp.segurancaVidaHumanaResposta = this.segurancaVidaHumanaCtrl.value;
+    this.componentes.segurancaVidaHumanaResposta = this.segurancaVidaHumanaCtrl.value;
+    this.rup.segurancaVidaHumanaResposta = this.segurancaVidaHumanaCtrl.value;
+    this.cascata.segurancaVidaHumanaResposta = this.segurancaVidaHumanaCtrl.value;
+
+    this.kanban.equipeIndependenteResposta = this.equipeIndependenteCtrl.value;
+    this.scrum.equipeIndependenteResposta = this.equipeIndependenteCtrl.value;
+    this.xp.equipeIndependenteResposta = this.equipeIndependenteCtrl.value;
+    this.componentes.equipeIndependenteResposta = this.equipeIndependenteCtrl.value;
+    this.rup.equipeIndependenteResposta = this.equipeIndependenteCtrl.value;
+    this.cascata.equipeIndependenteResposta = this.equipeIndependenteCtrl.value;
+
+    this.kanban.utilizacaoParcialResposta = this.utilizacaoParcialCtrl.value;
+    this.scrum.utilizacaoParcialResposta = this.utilizacaoParcialCtrl.value;
+    this.xp.utilizacaoParcialResposta = this.utilizacaoParcialCtrl.value;
+    this.componentes.utilizacaoParcialResposta = this.utilizacaoParcialCtrl.value;
+    this.rup.utilizacaoParcialResposta = this.utilizacaoParcialCtrl.value;
+    this.cascata.utilizacaoParcialResposta = this.utilizacaoParcialCtrl.value;
+
+    this.kanban.extensaoTempoResposta = this.extensaoTempoCtrl.value;
+    this.scrum.extensaoTempoResposta = this.extensaoTempoCtrl.value;
+    this.xp.extensaoTempoResposta = this.extensaoTempoCtrl.value;
+    this.componentes.extensaoTempoResposta = this.extensaoTempoCtrl.value;
+    this.rup.extensaoTempoResposta = this.extensaoTempoCtrl.value;
+    this.cascata.extensaoTempoResposta = this.extensaoTempoCtrl.value;
+
+    this.kanban.preReqResposta = this.preReqCtrl.value;
+    this.scrum.preReqResposta = this.preReqCtrl.value;
+    this.xp.preReqResposta = this.preReqCtrl.value;
+    this.componentes.preReqResposta = this.preReqCtrl.value;
+    this.rup.preReqResposta = this.preReqCtrl.value;
+    this.cascata.preReqResposta = this.preReqCtrl.value;
+
+    this.kanban.tamanhoEquipeResposta = this.tamanhoEquipeCtrl.value;
+    this.scrum.tamanhoEquipeResposta = this.tamanhoEquipeCtrl.value;
+    this.xp.tamanhoEquipeResposta = this.tamanhoEquipeCtrl.value;
+    this.componentes.tamanhoEquipeResposta = this.tamanhoEquipeCtrl.value;
+    this.rup.tamanhoEquipeResposta = this.tamanhoEquipeCtrl.value;
+    this.cascata.tamanhoEquipeResposta = this.tamanhoEquipeCtrl.value
+
+    this.kanban.donoDoProdutoResposta = this.donoDoProdutoCtrl.value;
+    this.scrum.donoDoProdutoResposta = this.donoDoProdutoCtrl.value;
+    this.xp.donoDoProdutoResposta = this.donoDoProdutoCtrl.value;
+    this.componentes.donoDoProdutoResposta = this.donoDoProdutoCtrl.value;
+    this.rup.donoDoProdutoResposta = this.donoDoProdutoCtrl.value;
+    this.cascata.donoDoProdutoResposta = this.donoDoProdutoCtrl.value;
+
+    this.kanban.multiFocoResposta = this.multiFocoCtrl.value;
+    this.scrum.multiFocoResposta = this.multiFocoCtrl.value;
+    this.xp.multiFocoResposta = this.multiFocoCtrl.value;
+    this.componentes.multiFocoResposta = this.multiFocoCtrl.value;
+    this.rup.multiFocoResposta = this.multiFocoCtrl.value;
+    this.cascata.multiFocoResposta = this.multiFocoCtrl.value;
+
+    this.kanban.documentacaoResposta = this.documentacaoCtrl.value;
+    this.scrum.documentacaoResposta = this.documentacaoCtrl.value;
+    this.xp.documentacaoResposta = this.documentacaoCtrl.value;
+    this.componentes.documentacaoResposta = this.documentacaoCtrl.value;
+    this.rup.documentacaoResposta = this.documentacaoCtrl.value;
+    this.cascata.documentacaoResposta = this.documentacaoCtrl.value;
+
+    this.kanban.escritorioResposta = this.escritorioCtrl.value;
+    this.scrum.escritorioResposta = this.escritorioCtrl.value;
+    this.xp.escritorioResposta = this.escritorioCtrl.value;
+    this.componentes.escritorioResposta = this.escritorioCtrl.value;
+    this.rup.escritorioResposta = this.escritorioCtrl.value;
+    this.cascata.escritorioResposta = this.escritorioCtrl.value;
+
+    this.kanban.linearResposta = this.linearCtrl.value;
+    this.scrum.linearResposta = this.linearCtrl.value;
+    this.xp.linearResposta = this.linearCtrl.value;
+    this.componentes.linearResposta = this.linearCtrl.value;
+    this.rup.linearResposta = this.linearCtrl.value;
+    this.cascata.linearResposta = this.linearCtrl.value;
+
+    this.kanban.probabilidadeResposta = this.probabilidadeCtrl.value;
+    this.scrum.probabilidadeResposta = this.probabilidadeCtrl.value;
+    this.xp.probabilidadeResposta = this.probabilidadeCtrl.value;
+    this.componentes.probabilidadeResposta = this.probabilidadeCtrl.value;
+    this.rup.probabilidadeResposta = this.probabilidadeCtrl.value;
+    this.cascata.probabilidadeResposta = this.probabilidadeCtrl.value;
+
+    return this.calculate();
+  }
+
+
+
+
+
+  private converteBoolean(valor: boolean): Booleana {
+    return valor ? Booleana.SIM : Booleana.NAO;
   }
 
   private alert(): void {
